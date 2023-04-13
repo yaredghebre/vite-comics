94dc4b7426b4a7cd3ea343c1a64b7f3576d91d47
yaredghebre <yared2391@gmail.com>
2023-04-13 16:33:14 -0300

    Add: AppMain Complete (Day 1)

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,9 +5,27 @@ export default {
 </script>
 
 <template>
-    <h2>Main</h2>
+    <main>
+        <div class="main-container container">
+            <h2> --> CONTENT GOES HERE &lt;-- </h2>
+        </div>
+    </main>
 </template>
 
 <style scoped lang="scss">
+@use "../style/partials/mixins" as *;
+@use "../style/partials/variables" as *;
+
+    main {
+        width: 100%;
+        height: 200px;
+        padding: 20px 0;
+        background-color: $secondary_color;
+        color: $fourth_color;
+
+        .main-container {
+            @include flex(row, space-between, center)
+        }
+    }
 </style>
 
