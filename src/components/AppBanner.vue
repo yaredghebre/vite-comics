aea4b453fd5a5c7e52c86d24b6705d16402f178b
yaredghebre <yared2391@gmail.com>
2023-04-13 17:28:16 -0300

    Add: AppBanner Complete (Day 1)

--- src/components/AppBanner.vue
+++ src/components/AppBanner.vue
@@ -1,13 +1,69 @@
 <script>
 export default {
-    name:"AppBanner"
+    name:"AppBanner",
+    data() {
+        return {
+            cards: [
+                {
+                    image:"src/assets/img/buy-comics-digital-comics.png",
+                    text:"DIGITAL COMICS"
+                },
+                {
+                    image:"src/assets/img/buy-comics-merchandise.png",
+                    text:"DC MERCHANDISE"
+                },
+                {
+                    image:"src/assets/img/buy-comics-subscriptions.png",
+                    text:"SUBSCRIPTION"
+                },
+                {
+                    image:"src/assets/img/buy-comics-shop-locator.png",
+                    text:"COMIC SHOP LOCATION"
+                },
+                {
+                    image:"src/assets/img/buy-dc-power-visa.svg",
+                    text:"DC POWER VISA"
+                },
+            ]
+
+        }
+    }
 }
 </script>
 
 <template>
-    <h2>Banner</h2>
+    <section class="banner-container ">
+        <div class="banner-row container">
+            <div class="banner-card" v-for="card in cards">
+                <img :src="card.image">
+                <span> {{ card.text }}</span>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style scoped lang="scss">
+@use "../style/partials/mixins" as *;
+@use "../style/partials/variables" as *;
+
+    .banner-container {
+        height: 200px;
+        color: $fourth_color;
+        background-color: $primary_color;
+        @include flex(row, space-between, center);
+
+        .banner-row {
+            height: 100px;
+            @include flex(row, space-between, center);
+
+            .banner-card {
+                width: 50px;
+                display: flex;
+                align-items: center;
+                gap: 20px;
+                font-size: 0.8rem;
+            }
+        }
+    }
 </style>
 
